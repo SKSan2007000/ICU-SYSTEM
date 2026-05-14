0f6f038cc77c014f5897f457a008649a910c281c
Santhosh <santhosh2007.axl@gmail.com>
2026-05-14 23:52:02 +0530

    deployment setup

--- icu_smartcare_django/icu_smartcare/settings.py
+++ icu_smartcare_django/icu_smartcare/settings.py
@@ -4,7 +4,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'capstone-demo-secret-key'
 DEBUG = True
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = [ALLOWED_HOSTS = ["icu_smartcare.onrender.com"]]
 
 INSTALLED_APPS = [
     'django.contrib.admin',
